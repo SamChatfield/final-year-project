bfa321af319e50ff477389590b0148176b4a3069
Sam Chatfield <samchatfield97@gmail.com>
2020-02-06 16:11:33 +0000

    Add CNNModel3 as a much more complex model. Add comments.

--- cnn.py
+++ cnn.py
@@ -4,38 +4,82 @@ from keras.layers import Conv1D, Conv2D, Activation, Dense, GlobalMaxPooling1D, 
 
 def CNNModel(input_shape):
     model = Sequential([
+        # Conv 1
         Conv1D(4, (3), input_shape=input_shape),
         Activation('relu'),
         GlobalMaxPooling1D(),
+        # Dense 1
         Dense(16),
         Activation('relu'),
+        # Output
         Dense(1),
         Activation('sigmoid')
     ])
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
 
     return model
 
 
 def CNNModel2(input_shape):
     model = Sequential([
+        # Conv 1
         Conv1D(4, (3), input_shape=input_shape),
         Activation('relu'),
         MaxPooling1D(),
+        # Conv 2
         Conv1D(8, (3)),
         Activation('relu'),
         GlobalMaxPooling1D(),
+        # Dense 1
         Dense(16),
         Activation('relu'),
+        # Output
         Dense(1),
         Activation('sigmoid')
     ])
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+
+    return model
+
+
+def CNNModel3(input_shape):
+    model = Sequential([
+        # Conv 1
+        Conv1D(32, 3, input_shape=input_shape),
+        Activation('relu'),
+        MaxPooling1D(),
+        # Conv 2
+        Conv1D(64, 3),
+        Activation('relu'),
+        GlobalMaxPooling1D(),
+        # Dense 1
+        Dense(128),
+        Activation('relu'),
+        # Dense 2
+        Dense(64),
+        Activation('relu'),
+        # Dense 3
+        Dense(32),
+        Activation('relu'),
+        # Output
+        Dense(1),
+        Activation('sigmoid')
+    ])
+
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
 
     return model

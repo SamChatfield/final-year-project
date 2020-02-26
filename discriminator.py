a0188a58336868961f0ea2b0b060c1db7ae09546
Sam Chatfield <samchatfield97@gmail.com>
2020-02-26 17:52:00 +0000

    Discriminator - add verbosity option and return final accuracy after initial training.

--- discriminator.py
+++ discriminator.py
@@ -6,13 +6,20 @@ from cnn import CNNModel3 as CNNModel
 
 class Discriminator:
     def __init__(
-        self, real_hmm, epoch_size, batch_size, sequence_length, pool_size=None
+        self,
+        real_hmm,
+        epoch_size,
+        batch_size,
+        sequence_length,
+        pool_size=None,
+        verbose=0,
     ):
         self._real_hmm = real_hmm
         self._epoch_size = epoch_size
         self._batch_size = batch_size
         self._sequence_length = sequence_length
         self._pool_size = pool_size
+        self._verbose = verbose
 
         self._train_data_generator = utils.HMMDataGenerator(
             self._real_hmm, self._epoch_size, self._batch_size, self._sequence_length
@@ -22,13 +29,19 @@ class Discriminator:
 
     def initial_train(self, epochs):
         if self._pool_size:
-            self._model.fit_generator(
+            history = self._model.fit_generator(
                 generator=self._train_data_generator,
                 epochs=epochs,
+                verbose=self._verbose,
                 use_multiprocessing=True,
                 workers=self._pool_size,
             )
         else:
-            self._model.fit_generator(
-                generator=self._train_data_generator, epochs=epochs
+            history = self._model.fit_generator(
+                generator=self._train_data_generator,
+                epochs=epochs,
+                verbose=self._verbose,
             )
+
+        final_acc = history.history["accuracy"][-1]
+        return final_acc

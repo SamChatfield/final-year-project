b4878b60a6435461f05f94736e9b3e2ec0904bae
Sam Chatfield <samchatfield97@gmail.com>
2020-02-26 17:53:53 +0000

    run - create experiment to encapsulate multiple runs with the same paramaters. Save the results to JSON files so we can load and graph each experiment later.

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import json
 import string
+from datetime import datetime
 
 import deap
 import numpy as np
@@ -8,60 +10,100 @@ from discriminator import Discriminator
 from ea import EA
 
 DEFAULT_PARAMS = {
+    # Algorithm Parameters
     "states": 3,
     "symbols": 5,
     "epochs": 10,
     "epoch_size": 500,
     "batch_size": 200,
     "seq_len": 20,
-    "pool_size": 8,
-    "pop_size": 100,
+    "pop_size": 50,
     "gens": 50,
     "offspring_prop": 0.25,
     "cx_prob": 0.0,
     "mut_prob": 1.0,
+    # Implementation Parameters
+    "_pool_size": 4,
 }
 
 
 def param_assert(params):
     assert params["states"] > 0
     assert 0 < params["symbols"] <= 26
+    assert 0.0 <= params["offspring_prop"] <= 1.0
+    assert 0.0 <= params["cx_prob"] <= 1.0
+    assert 0.0 <= params["mut_prob"] <= 1.0
 
 
-def run(params):
+def run(param_subset):
+    # Overwrite the default values of the provided parameters
+    params = {**DEFAULT_PARAMS, **param_subset}
+
     print(params)
     param_assert(params)
 
     x = params["states"]
     y = string.ascii_lowercase[: params["symbols"]]
     s = [1.0] + [0.0] * (x - 1)
+    # Random HMM that will act as the 'true' underlying distribution
     real_hmm = hmm.random_hmm(x, y, s)
+    # Different random HMM that will be used to benchmark the best solution we find
+    rand_hmm = hmm.random_hmm(x, y, s)
 
     d = Discriminator(
         real_hmm,
         params["epoch_size"],
         params["batch_size"],
         params["seq_len"],
-        pool_size=params["pool_size"],
+        pool_size=params["_pool_size"],
+        verbose=2,
     )
 
-    d.initial_train(params["epochs"])
+    print(f"Pre-training discriminator...")
+    acc = d.initial_train(params["epochs"])
+    print(f"Pre-trained discriminiator accuracy: {acc}")
 
     g = EA(d, params["pop_size"], states=x, symbols=len(y))
 
-    final_pop, hall_of_fame = g.run(
+    print(f"Running generator...")
+    final_pop, _, logbook = g.run(
         params["gens"], params["offspring_prop"], params["cx_prob"], params["mut_prob"],
     )
 
     best_ind = deap.tools.selBest(final_pop, 1)[0]
     best_hmm = hmm.HMM(x, np.array(list(y)), best_ind[0], best_ind[1], np.array(s))
 
-    rand_hmm = hmm.random_hmm(x, y, s)
+    return real_hmm, best_hmm, rand_hmm, logbook
 
-    best_l2 = hmm.total_l2_diff(real_hmm, best_hmm)
-    rand_l2 = hmm.total_l2_diff(real_hmm, rand_hmm)
 
-    return real_hmm, best_hmm, best_l2, rand_l2
+def experiment(params, runs):
+    mean_fitnesses = []
+    best_l2s = []
+    rand_l2s = []
+
+    for i in range(runs):
+        print(f"Run {i+1}")
+        real_hmm, best_hmm, rand_hmm, logbook = run(params)
+
+        best_l2 = hmm.total_l2_diff(real_hmm, best_hmm)
+        rand_l2 = hmm.total_l2_diff(real_hmm, rand_hmm)
+
+        mean_fitnesses.append(logbook.select("mean"))
+        best_l2s.append(best_l2)
+        rand_l2s.append(rand_l2)
+
+    exp_data = {
+        "params": params,
+        "mean_fitnesses": mean_fitnesses,
+        "best_l2s": best_l2s,
+        "rand_l2s": rand_l2s,
+    }
+
+    exp_file = f'experiments/exp_{datetime.now().strftime("%y%m%d-%H%M%S")}.json'
+    with open(exp_file, "w") as f:
+        json.dump(exp_data, f, indent=4)
+
+    return exp_data
 
 
 def main():
